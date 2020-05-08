2e186127e449059071359cbe31ec5a33e93d9b79
Rianna <rianna.cleary@gmail.com>
2020-05-08 16:22:24 -0400

    adding hooks to start navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState} from 'react';
+import {withRouter} from 'react-router-dom';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [isMenuOpen, setIsMenuOpen] = userState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>
-          BEAT 🎧 MAKER 
+          BEAT 🎧 MAKER
         </h1>
       </header>
     </div>
-  );
+
+
+
 }
 
-export default App;
+export default withRouter(App);
+
+
+// return (
+//   <div className="App">
+//     <header className="App-header">
+//       <h1>
+//         BEAT 🎧 MAKER
+//       </h1>
+//     </header>
+//   </div>
