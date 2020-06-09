c7644a29886d5fe0a4216fb545e47172acc96004
Rianna <rianna.cleary@gmail.com>
2020-06-09 17:32:11 -0400

    plays sound

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
-// import {withRouter} from 'react-router-dom';
-// import {Switch, Route} from 'react-router'
 import AlienPad from './components/AlienPad'
 import Emoji from './components/Emoji'
 import './App.css';
 
-const martianScanner = new Audio('martianScanner.mp3')
+// const martianScanner = new Audio('martianScanner.mp3')
+
+const data = [
+  {id: 'martianScanner', letter: 'Q', audio: 'martianScanner.mp3'}
+]
 
 class App extends React.Component {
 
-  data = [
-    {id: 'martianScanner', letter: 'Q', audio: 'martianScanner.mp3'}
-  ]
+
 
   render() {
+    console.log(data.audio)
     return (
       <div className="App">
         <header>
           <h1 className='header'>Martian <Emoji symbol='👽' label='headphones'/> Pad</h1>
         </header>
-        {this.data.map(d => (
+        {data.map(d => (
           <AlienPad
           id={d.id}
           letter={d.letter}
-          src={d.audio}
+          audio={d.audio}
           />
         ))}
       </div>
--- src/components/AlienPad.js
+++ src/components/AlienPad.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 
 class AlienPad extends React.Component {
+
+  handleClick = () => {
+    this.audio.play()
+    this.audio.currentTime = 0
+  }
+
+
   render() {
+    let {id, letter, audio} = this.props
     console.log(this.props)
     return(
-      <div className='alien-pad' id={this.props.id}>
-
+      <div className='alien-pad'
+      id={this.props.id}
+      onClick={this.handleClick}
+      >
+        <h1>{letter}</h1>
+        <audio
+        ref={ref => this.audio = ref}
+        src={audio}
+        id={id}
+        >
+        </audio>
       </div>
     )
   }
